b10599c6cc159f316f43c6f09331ba5e71b74202
Wizard-F <forrest.futao.wei@gmail.com>
2022-04-16 21:47:04 +0800

    Rewrite skeleton code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import PageWithNavbar from "./components/PageWithNavbar";
-import CourseFilterAndCourseList from "./components/CourseFilterAndCourseList";
+import Homepage from "./components/Homepage";
 import CoursePage from "./components/CoursePage";
-import LoginForm from "./components/LoginForm";
-import ReviewForm from "./components/ReviewForm";
-
+import CreateReviewPage from "./components/CreateReviewPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageWithNavbar />} >
-          {/* Web pages of the following routes have a navbar at the top */}
 
-          <Route index element={<CourseFilterAndCourseList />} />
+        <Route path="/" element={<Homepage />} />
 
-          <Route path="courses/:courseId">
-            <Route index element={<CoursePage />} />
-            <Route path="new-review" element={<ReviewForm />} /> {/* Route for writing a new review with auto-filled course */}
-          </Route>
-          
-          <Route path="login" element={<LoginForm />} />
-
-          <Route path="new-review" element={<ReviewForm />} /> {/* Route for writing a review without auto-filled course */}
+        <Route path="courses/:courseId">
+          <Route index element={<CoursePage />} />
+          <Route path="new-review" element={<CreateReviewPage />} /> {/* Route for writing a new review with auto-filled course info */}
         </Route>
+
+        <Route path="new-review" element={<CreateReviewPage />} /> {/* Route for writing a review without auto-filled course info */}
+        
       </Routes>
     </BrowserRouter>
   );
