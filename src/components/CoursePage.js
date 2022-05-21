d56546edb52596fea3a6dc914f45d02a6b210b31
Wizard-F <forrest.futao.wei@gmail.com>
2022-05-21 18:39:46 +0800

    Implement Navbar

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,4 +1,10 @@
+import Navbar from "./Navbar";
+
 
 export default function CoursePage() {
-  
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,22 @@
+import RB_Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 export default function Navbar() {
   
+  return (
+    <RB_Navbar bg="dark" variant="dark">
+      <Container>
+        <RB_Navbar.Brand>JCourses</RB_Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/new-review">New Review</Nav.Link>
+          <Nav.Link href="/login">JAccount Login</Nav.Link>
+        </Nav>
+      </Container>
+    </RB_Navbar>
+  );
 };
