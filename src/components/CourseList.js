6f9d626132cf27eaacf23e4239f45ae10de6dc11
Wizard-F <forrest.futao.wei@gmail.com>
2022-05-22 11:07:09 +0800

    Implement CourseList

---
+++ src/components/CourseList.js
@@ -0,0 +1,23 @@
+import Spinner from "react-bootstrap/Spinner";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
+export default function CourseList(props) {
+  return (
+    <Container className="p-3 border rounded-3">
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <Card>
+        <Card.Body>
+          EE458: Software Engineering
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          CS3302: Database
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
