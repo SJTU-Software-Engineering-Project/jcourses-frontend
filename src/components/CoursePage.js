21d22b67f0a5c34795aaadf18c2fa0f6fc0ff5da
Wizard-F <forrest.futao.wei@gmail.com>
2022-05-27 14:37:02 +0800

    Implement data-fetch logic

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,7 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import { useParams } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+
+import { API_URL } from "../utils/config";
 
 export default function CoursePage() {
+  const {courseId} = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [course, setCourse] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${API_URL}/courses/${courseId}`);
+        setCourse(response.data.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+    
+  }, [courseId]);
+
   return (
-    <>
-    </>
+    <Container className="m-3 p-3">
+      {
+        isLoading ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : (
+          <Container>
+            Course Code: {course?.code}
+            Course Name: {course?.name}
+            Teacher: {course?.teacher}
+          </Container>
+        )
+      }
+    </Container>
   );
 };
